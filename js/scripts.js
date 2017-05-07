f8afbbbf280ba94be5c4110dbb318c46df19879a
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-05-07 11:23:10 +0200

    Add next and prev controls

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,52 @@
 $(function () {
     var carouselList = $("#carousel ul"),
+        carouselInterval,
         carouselElem = $('#carousel'),
-        carouselWidth = parseInt(carouselElem.css('width'), 10);
-    setInterval(changeSlide, 3000);
-    function changeSlide() {
-        carouselList.animate({'marginLeft': -carouselWidth}, 500, moveFirstSlide);
+        carouselWidth = parseInt(carouselElem.css('width'), 10),
+        firstItem = carouselList.find("li:first"),
+        lastItem = carouselList.find("li:last"),
+        leftControl = $(".left"),
+        rightControl = $(".right"),
+        carouselControls = $(".control");
+    function runCarousel() {
+        carouselList.animate({'marginLeft': -carouselWidth * 2}, 500, function () {
+            moveSlide("right");
+        });
     }
-    function moveFirstSlide() {
-        var firstItem = carouselList.find("li:first"),
-            lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);
-        carouselList.css({marginLeft: 0});
+    function setCarouselInterval() {
+        carouselInterval = setInterval(runCarousel, 3000);
     }
+    setCarouselInterval();
+    firstItem.before(lastItem);
+    carouselList.css({marginLeft: -carouselWidth});
+    function moveSlide(direction) {
+        firstItem = carouselList.find("li:first");
+        lastItem = carouselList.find("li:last");
+        if (direction == "right") {
+            lastItem.after(firstItem);
+        } else if (direction == "left") {
+            firstItem.before(lastItem);
+        }
+        carouselList.css({marginLeft: -carouselWidth});
+    }
+    leftControl.click(function (event) {
+        event.preventDefault();
+        clearInterval(carouselInterval);
+        carouselList.animate({'marginLeft': 0}, 500, function () {
+            moveSlide("left");
+        });
+    });
+    rightControl.click(function (event) {
+        event.preventDefault();
+        clearInterval(carouselInterval);
+        runCarousel();
+    });
+    carouselControls.hover(
+        function () {
+            clearInterval(carouselInterval);
+        },
+        function () {
+            setCarouselInterval();
+        }
+    );
 });
