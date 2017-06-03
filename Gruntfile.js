4e4bef98f7e81dbc61afae0cd59154e890a6701f
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-06-03 15:16:29 +0200

    Change js readme and html

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function (grunt) {
                     expand: true,
                     cwd: 'img/',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: 'img/images/'
+                    dest: 'images/'
                 }]
             }
         },
