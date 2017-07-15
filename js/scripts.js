cd25908f35f1c82a562469fe0ddeca7ad50c126f
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-07-15 10:09:13 +0200

    Optimize js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(function () {
-    var carouselList = $("#carousel ul"),
+    var $carouselList = $("#carousel ul"),
         carouselInterval,
-        carouselElem = $('#carousel'),
-        carouselWidth = parseFloat(carouselElem.css('width'), 10),
-        firstItem = carouselList.find("li:first"),
-        lastItem = carouselList.find("li:last"),
-        leftControl = $(".left"),
-        rightControl = $(".right"),
-        carouselControls = $(".control");
+        $carouselElem = $('#carousel'),
+        carouselWidth = parseFloat($carouselElem.css('width'), 10),
+        $firstItem = $carouselList.find("li:first"),
+        $lastItem = $carouselList.find("li:last"),
+        $leftControl = $(".left"),
+        $rightControl = $(".right"),
+        $carouselControls = $(".control");
     function runCarousel() {
-        carouselList.animate({'marginLeft': -carouselWidth * 2}, 500, function () {
+        $carouselList.animate({'marginLeft': -carouselWidth * 2}, 500, function () {
             moveSlide("right");
         });
     }
@@ -17,31 +17,31 @@ $(function () {
         carouselInterval = setInterval(runCarousel, 3000);
     }
     setCarouselInterval();
-    firstItem.before(lastItem);
-    carouselList.css({marginLeft: -carouselWidth});
+    $firstItem.before($lastItem);
+    $carouselList.css({marginLeft: -carouselWidth});
     function moveSlide(direction) {
-        firstItem = carouselList.find("li:first");
-        lastItem = carouselList.find("li:last");
+        $firstItem = $carouselList.find("li:first");
+        $lastItem = $carouselList.find("li:last");
         if (direction == "right") {
-            lastItem.after(firstItem);
+            $lastItem.after($firstItem);
         } else if (direction == "left") {
-            firstItem.before(lastItem);
+            $firstItem.before($lastItem);
         }
-        carouselList.css({marginLeft: -carouselWidth});
+        $carouselList.css({marginLeft: -carouselWidth});
     }
-    leftControl.click(function (event) {
+    $leftControl.click(function (event) {
         event.preventDefault();
         clearInterval(carouselInterval);
-        carouselList.animate({'marginLeft': 0}, 500, function () {
+        $carouselList.animate({'marginLeft': 0}, 500, function () {
             moveSlide("left");
         });
     });
-    rightControl.click(function (event) {
+    $rightControl.click(function (event) {
         event.preventDefault();
         clearInterval(carouselInterval);
         runCarousel();
     });
-    carouselControls.hover(
+    $carouselControls.hover(
         function () {
             clearInterval(carouselInterval);
         },
@@ -51,7 +51,7 @@ $(function () {
     );
     $(window).resize(function () {
         clearInterval(carouselInterval);
-        carouselWidth = parseFloat(carouselElem.css('width'), 10);
+        carouselWidth = parseFloat($carouselElem.css('width'), 10);
         setCarouselInterval();
     });
 });
