eca3feda96024e81e6e5a7e1f2e5f2e252fbc63b
Mariusz Dziecielski <mariuszdziec@gmail.com>
2017-09-10 13:35:11 +0200

    Remove unnecessary spaces & change js

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,62 @@
 $(function () {
-    const $carouselList = $("#carousel ul"),
-        $carouselElem = $('#carousel'),
-        $leftControl = $(".left"),
-        $rightControl = $(".right"),
-        $carouselControls = $(".control");
-    let carouselWidth = parseFloat($carouselElem.css('width'), 10),
-        $firstItem = $carouselList.find("li:first"),
-        $lastItem = $carouselList.find("li:last"),
-        carouselInterval;
-    function runCarousel() {
-        $carouselList.animate({'marginLeft': -carouselWidth * 2}, 500, function () {
-            moveSlide("right");
+    function Carousel(element) {
+        this.$carouselElem = element;
+        this.$carouselList = $("#carousel ul");
+        this.$leftControl = $(".left");
+        this.$rightControl = $(".right");
+        this.$carouselControls = $(".control");
+        this.carouselWidth = parseFloat(this.$carouselElem.css('width'), 10);
+        this.$firstItem = this.$carouselList.find("li:first");
+        this.$lastItem = this.$carouselList.find("li:last");
+        this.carouselInterval = 0;
+        this.$leftControl.click(event => {
+            event.preventDefault();
+            clearInterval(this.carouselInterval);
+            this.$carouselList.animate({'marginLeft': 0}, 500, () => {
+                this.moveSlide("left");
+            });
+        });
+        this.$rightControl.click(event => {
+            event.preventDefault();
+            clearInterval(this.carouselInterval);
+            this.runCarousel();
+        });
+        this.$carouselControls.hover(
+            () => {
+                clearInterval(this.carouselInterval);
+            },
+            () => {
+                this.setCarouselInterval();
+            }
+        );
+        $(window).resize(() => {
+            clearInterval(this.carouselInterval);
+            this.carouselWidth = parseFloat(this.$carouselElem.css('width'), 10);
+            this.setCarouselInterval();
         });
     }
-    function setCarouselInterval() {
-        carouselInterval = setInterval(runCarousel, 3000);
-    }
-    setCarouselInterval();
-    $firstItem.before($lastItem);
-    $carouselList.css({marginLeft: -carouselWidth});
-    function moveSlide(direction) {
-        $firstItem = $carouselList.find("li:first");
-        $lastItem = $carouselList.find("li:last");
-        if (direction == "right") {
-            $lastItem.after($firstItem);
-        } else if (direction == "left") {
-            $firstItem.before($lastItem);
-        }
-        $carouselList.css({marginLeft: -carouselWidth});
-    }
-    $leftControl.click(function (event) {
-        event.preventDefault();
-        clearInterval(carouselInterval);
-        $carouselList.animate({'marginLeft': 0}, 500, function () {
-            moveSlide("left");
-        });
-    });
-    $rightControl.click(function (event) {
-        event.preventDefault();
-        clearInterval(carouselInterval);
-        runCarousel();
-    });
-    $carouselControls.hover(
-        function () {
-            clearInterval(carouselInterval);
+    Carousel.prototype = {
+        runCarousel: function () {
+            carousel.$carouselList.animate({'marginLeft': -carousel.carouselWidth * 2}, 500, () => {
+                carousel.moveSlide("right");
+            });
         },
-        function () {
-            setCarouselInterval();
+        setCarouselInterval: function () {
+            this.carouselInterval = setInterval(this.runCarousel, 3000);
+        },
+        moveSlide: function (direction) {
+            this.$firstItem = this.$carouselList.find("li:first");
+            this.$lastItem = this.$carouselList.find("li:last");
+            if (direction == "right") {
+                this.$lastItem.after(this.$firstItem);
+            } else if (direction == "left") {
+                this.$firstItem.before(this.$lastItem);
+            }
+            this.$carouselList.css({marginLeft: -this.carouselWidth});
         }
-    );
-    $(window).resize(function () {
-        clearInterval(carouselInterval);
-        carouselWidth = parseFloat($carouselElem.css('width'), 10);
-        setCarouselInterval();
-    });
+    };
+    const carousel = new Carousel($('#carousel'));
+    carousel.setCarouselInterval();
+    carousel.$firstItem.before(carousel.$lastItem);
+    carousel.$carouselList.css({marginLeft: -carousel.carouselWidth});
 });
